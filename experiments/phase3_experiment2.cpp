5d847be02e2a17b630e75925d804a12aca5a4ca1
jasonliu-json <46848142+jasonliu-json@users.noreply.github.com>
2023-04-10 21:35:26 -0400

    oof

--- experiments/phase3_experiment2.cpp
+++ experiments/phase3_experiment2.cpp
@@ -21,7 +21,7 @@ int main() {
     std::vector<int> pairs_inserted;
 
     size_t memtable_size = 10485760;                            // This is 10 MB
-    string policy = "lru";
+    string policy = "clock";
     int initial_num_bits = 2;
     int max_bp_size = 10485760;                                 // This is 10 MB
     int maximum_num_items_threshold = 5000;
@@ -46,14 +46,15 @@ int main() {
         std::cout << "Inserted " << (i) << " key-value pairs or " << (i * 100 / num_kv_pairs_gigabyte) << "% so far, this chunck took " << chunk_time_put << " ms" << std::endl;
 
         auto chunk_start_get = std::chrono::high_resolution_clock::now();
-        for (int j = 0; j < 10000; j+=10) {
-            db_key_t key = i + j;
-            db_val_t val = db.get(key);
+        for (int j = 0; j < 1000; j++) {
+            // get ranom key that is between 0 and i
+            db_key_t key = rand() % (i + chunk_size);
+            db.get(key);
         }
         auto chunk_end_get = std::chrono::high_resolution_clock::now();
         auto chunk_time_get = std::chrono::duration_cast<std::chrono::milliseconds>(chunk_end_get - chunk_start_get).count();
         get_runtimes.push_back(chunk_time_get);
-        std::cout << "Got key " << 0 << " which took " << chunk_time_get << " ms" << std::endl;
+//        std::cout << "Got key " << 0 << " which took " << chunk_time_get << " ms" << std::endl;
 
         auto chunk_start_scan = std::chrono::high_resolution_clock::now();
         db.scan(0, num_kv_pairs_gigabyte);
@@ -63,6 +64,8 @@ int main() {
         std::cout << "Scanned all keys which took " << chunk_time_scan << " ms" << std::endl;
     }
 
+    std::cout << "Finished inserting " << num_kv_pairs_gigabyte << " key-value pairs" << std::endl;
+
     double sum = 0.0;
     for (size_t i = 0; i < put_runtimes.size(); ++i) {
         sum += put_runtimes[i];
@@ -70,10 +73,8 @@ int main() {
     }
 
 
-    string filename = "experiments/phase3_put_runtimes.csv";
-
     std::ofstream result_file_2;
-    result_file_2.open("experiments/phase3_get_runtimes_2.csv");
+    result_file_2.open("experiments/phase3_get_runtimes_numbits_5.csv");
     result_file_2 << "Volume of Key-Value Pairs,Get Runtime\n";
     for (int i = 0; i < pairs_inserted.size(); ++i) {
         result_file_2 << pairs_inserted[i] << "," << get_runtimes[i] << "\n";
@@ -81,7 +82,7 @@ int main() {
     result_file_2.close();
 
     std::ofstream result_file_3;
-    result_file_3.open("experiments/phase3_scan_runtimes_2.csv");
+    result_file_3.open("experiments/phase3_scan_runtimes_numbits_5.csv");
     result_file_3 << "Volume of Key-Value Pairs,Scan Runtime\n";
     for (int i = 0; i < pairs_inserted.size(); ++i) {
         result_file_3 << pairs_inserted[i] << "," << scan_runtimes[i] << "\n";
